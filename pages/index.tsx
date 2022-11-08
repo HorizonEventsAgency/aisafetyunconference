a54da8064a3f37c7b27fe00af31800b9afa13b66
Orpheus Lummis <o@orpheuslummis.info>
2022-11-08 12:21:05 +0100

    update front page with latest info

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,58 +21,25 @@ const Home: NextPage = () => {
             <Image src={banner} alt="New Orleans cityscape" />
 
             <Section title="Status">
-                <p>
-                    NeurIPS is online-only in 2020 and 2021 because of the COVID-19 pandemic. The AISAU is designed to be an in-person event, therefore not happening in these years. NeurIPS 2022 is planned to be <a href="https://blog.neurips.cc/2021/07/02/neurips-2021-deadline-extension-2/">in-person in New Orleans</a>. We hope for the unconference to happen then and there.
-                </p>
+                As of 2022, the AI Safety Unconference is now organized by the <i><a href="https://aisafetyevents.org/">AI Safety Events</a></i> organization.
+                Please find the latest events information at <a href="https://aisafetyevents.org/events/">aisafetyevents.org/events/</a>.
             </Section>
 
             <Section title="Description">
                 <p>
-                The AI Safety and Alignment Unconference brings together persons interested in all aspects of AI safety and alignment, from technical AI safety problems to issues of governance and responsible use of AI, for one day during the NeurIPS week. As an unconference, the event aims to maximize valuable social interactions between participants, through free-form discussions, moderated discussion groups, one-on-ones, and lightning talks.
+                    The AI Safety and Alignment Unconference brings together persons interested in all aspects of AI safety and alignment, from technical AI safety problems to issues of governance and responsible use of AI, for one day during the NeurIPS week. As an unconference, the event aims to maximize valuable social interactions between participants, through free-form discussions, moderated discussion groups, one-on-ones, and lightning talks.
                 </p>
             </Section>
 
-            <Section title="Participation">
-                <p>
-                    <b><a href="https://groups.google.com/forum/#!forum/ai-safety-unconference/join">Join the email group</a></b>, and…
-                    <ul>
-                        <li>Start a thread for any topic you are interested in discussing.</li>
-                        <li>Indicate if you’re interested in moderating or leading a discussion group.</li>
-                        <li>Suggest background readings or share your thoughts on existing topics… What are you curious about?  What opinions or perspectives do you want to promote or challenge?</li>
-                        <li>Post in the “one-on-one” thread and let other participants know which topics you are excited to discuss individually. As examples, last year’s discussion topics included “<i>Establishing trust in advanced AI systems</i>”, “<i>Concrete threat models</i>”, and “<i>Governance, policy, strategy</i>”.</li>
-                    </ul>
-                </p>
-
-                <p><b>Give a Lightning Talk</b>: Lightning talks (5-6 minutes in length) take place during the lunch hour.</p>
-
-                <p><b>Background readings</b>: We recommend <a href="https://vkrakovna.wordpress.com/ai-safety-resources/">Victoria Krakovna&apos;s AI safety resources</a> and the <a href="https://rohinshah.com/alignment-newsletter/">Alignment newsletter</a> as outlooks of the field.</p>
-            </Section>
-
-            <Section title="Testimonials">
-                <ul>
-                    <li><i>A great way to meet the best people in the area and propel daring ideas forward.</i> — <a href="https://www.fhi.ox.ac.uk/team/stuart-armstrong/">Stuart Armstrong</a></li>
-                    <li><i>The event was a great place to meet others with shared research interests. I particularly enjoyed the small discussion groups that exposed me to new perspectives.</i> — <a href="https://www.gleave.me/">Adam Gleave</a></li>
-                </ul>
-            </Section>
-
-            <Section title="Organisers">
-                <ul>
-                    <li>David Krueger</li>
-                    <li>Orpheus Lummis</li>
-                    <li>Joe Collman</li>
-                </ul>
-                <p>If you have any questions or thoughts, reach out to <a href="mailto:contact@aisafetyunconference.info">contact@aisafetyunconference.info</a>. If you&apos;re interested in organising the unconference, let&apos;s chat! You can send us <a href="https://docs.google.com/forms/d/e/1FAIpQLSe8XxXu1ma6ytGTAHAOglofr92Qq_YSttanBLJqqZ-jkcMfSw/viewform">anonymous feedback</a>.</p>
-            </Section>
-
-            <Section title="Previous years">
+            <Section title="Previous events">
                 <ul>
                     <li><Link href="/2019"><a>2019</a></Link></li>
                     <li><Link href="/2018"><a>2018</a></Link></li>
                 </ul>
             </Section>
 
-            <p id="lastupdate">(last update: 2021-10-07)</p>
-            </>
+            <p id="lastupdate">(last update: 2022-11-08)</p>
+        </>
     )
 }
 
