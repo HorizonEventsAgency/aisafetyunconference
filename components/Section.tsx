2a871078688f4f55babae2f5eec0e8b12b225053
Orpheus Lummis <o@orpheuslummis.info>
2023-11-27 10:40:08 +0545

    working with latest packages

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,16 +1,18 @@
-import { NextPage } from "next"
+import { NextPage } from "next";
+import { ReactNode } from "react";
 
 interface Props {
-  title: string
+  title: string;
+  children: ReactNode;
 }
 
 const Section: NextPage<Props> = (props) => {
-    return (
-        <div >
-            <h2 className="text-2xl my-5 mb-2">{props.title}</h2>
-            {props.children}
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2 className="text-2xl my-5 mb-2">{props.title}</h2>
+      {props.children}
+    </div>
+  );
+};
 
-export default Section
+export default Section;
